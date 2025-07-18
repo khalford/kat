ef35abd22d63ac88d5858c040d1ee15a0e2a766c
Kalibh Halford <kalibh.halford@stfc.ac.uk>
2025-07-18 12:28:54 +0100

    LINT: Changes to code to make it pass clang-tidy

--- main.cpp
+++ main.cpp
@@ -1,76 +1,80 @@
-#include <iostream>
-#include <fstream>
 #include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <span>
+#include <cstddef>
+#include <string>
 
 using namespace std;
 using namespace filesystem;
 
-
-int read_file(const path& _path, const int argc, char *argv[]) {
-    bool count = false;
-    int i = 1;
-    for (i=1; i < argc; i++) {
-        const auto option = string(argv[i]);
-        if (!option.starts_with("-")){continue;}
-        if (option == "--count" || option == "-c") {
-            count = true;
-            continue;
-        }
-        cout << "Unsupported option: " << option << endl;
+auto read_file(const path &_path, const int argc, span<char*> args) -> int {
+  bool count = false;
+  for (size_t i = 1; i < argc; i++) {
+    const auto option = string(args[i]);
+    if (option.empty() || option[0] != '-') {
+      continue;
     }
-
-
-    ifstream user_file;
-    user_file.open(_path.string());
-
-    if (!user_file.is_open()) {
-        cout << "Unable to open file " << absolute(_path) << endl;
-        return 1;
+    if (option == "--count" || option == "-c") {
+      count = true;
+      continue;
     }
+    cout << "Unsupported option: " << option << "\n";
+  }
 
-    string line;
-    cout << "Contents of " << absolute(_path) << ":" << endl;
+  ifstream user_file;
+  user_file.open(_path.string());
 
-    if (count == true) {
-        int _line_num = 1;
-        while (getline (user_file, line)) {
-            cout << _line_num << "  " << line << endl;
-            ++_line_num;
-        }
-    } else {
-        while (getline (user_file, line)) {
-            cout << line << endl;
-        }
+  if (!user_file.is_open()) {
+    cout << "Unable to open file " << absolute(_path) << "\n";
+    return 1;
+  }
+
+  string line;
+  cout << "Contents of " << absolute(_path) << ":" << "\n";
+
+  if (count) {
+    int _line_num = 1;
+    while (getline(user_file, line)) {
+      cout << _line_num << "  " << line << "\n";
+      ++_line_num;
     }
+  } else {
+    while (getline(user_file, line)) {
+      cout << line << "\n";
+    }
+  }
 
-    user_file.close();
-    return 0;
+  user_file.close();
+  return 0;
 }
 
-int main(const int argc, char *argv[]) {
-    // Check if any arguments are provided
-    if (argc < 2) {
-        cout << "Please provide a file path. Usage: kat <filename>" << endl;
-        return 1;
-    }
+auto main(const int argc, char *argv[]) -> int {
+  // Check if any arguments are provided
+  if (argc < 2) {
+    cout << "Please provide a file path. Usage: kat <filename>" << "\n";
+    return 1;
+  }
 
-    // Find which argument isn't a flag and use it as the path
-    path file_path;
-    int i = 1;
-    bool path = false;
-    for (i=1; i < argc; i++) {
-        const auto option = string(argv[i]);
-        if (!option.starts_with("-")) {
-            file_path=option;
-            path = true;
-        }
-    }
-    if (!path) {
-        cout << "Please provide a file path. Usage: kat <filename>" << endl;
-        return 1;
-    }
+  span<char*> args(argv, static_cast<size_t>(argc));
 
-    // Read the file
-    return read_file(file_path, argc, argv);
+  // Find which argument isn't a flag and use it as the path
+  path file_path;
+  bool _path = false;
+  for (size_t i = 1; i < argc; i++) {
+    const auto option = string(args[i]);
+    if (!option.starts_with("-")) {
+      file_path = option;
+      _path = true;
+    }
+  }
+  if (!_path) {
+    cout << "Please provide a file path. Usage: kat <filename>" << "\n";
+    return 1;
+  }
 
+
+  // Read the file
+  return read_file(file_path, argc, args);
 }
